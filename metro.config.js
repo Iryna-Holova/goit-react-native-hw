c8ea796e91eb2af25ef7b230d9797dd2486a841a
Iryna Holova <gis.golova@gmail.com>
2024-12-12 14:25:18 +0200

    bugfix

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
-import { getDefaultConfig } from "expo/metro-config";
+const { getDefaultConfig } = require("expo/metro-config");
 
-export default (() => {
+module.exports = (() => {
   const config = getDefaultConfig(__dirname);
 
   config.resolver.assetExts = config.resolver.assetExts.filter(
